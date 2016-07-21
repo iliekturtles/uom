f870af33b31188227d873051f79b2931c36fb372
Mike Boutin <mike.boutin@gmail.com>
2016-07-21 07:44:17 -0400

    Remove `si::units` module and move quantities directly into `si`.

--- src/si/mod.rs
+++ src/si/mod.rs
@@ -1,10 +1,22 @@
-mod units;
-
 pub mod prefix;
 
-use ::{Scalar};
+pub mod amount_of_substance;
+pub mod electric_current;
+pub mod length;
+pub mod luminous_intensity;
+pub mod mass;
+pub mod thermodynamic_temperature;
+pub mod time;
+pub mod velocity;
 
-pub use self::units::*;
+pub use self::amount_of_substance::*;
+pub use self::electric_current::*;
+pub use self::length::*;
+pub use self::luminous_intensity::*;
+pub use self::mass::*;
+pub use self::thermodynamic_temperature::*;
+pub use self::time::*;
+pub use self::velocity::*;
 
 system!(SI:
     dimensions {
@@ -17,8 +29,24 @@ system!(SI:
         luminous_intensity: J, cd;
     });
 
-pub type Time<V> = Scalar<SI<Z0, Z0, P1, Z0, Z0, Z0, Z0>, V>;
-pub type ElectricCurrent<V> = Scalar<SI<Z0, Z0, Z0, P1, Z0, Z0, Z0>, V>;
-pub type ThermodynamicTemperature<V> = Scalar<SI<Z0, Z0, Z0, Z0, P1, Z0, Z0>, V>;
-pub type AmountOfSubstance<V> = Scalar<SI<Z0, Z0, Z0, Z0, Z0, P1, Z0>, V>;
-pub type LuminousIntensity<V> = Scalar<SI<Z0, Z0, Z0, Z0, Z0, Z0, P1>, V>;
+pub mod f32 {
+    pub type AmountOfSubstance = super::AmountOfSubstance<f32>;
+    pub type ElectricCurrent = super::ElectricCurrent<f32>;
+    pub type Length = super::Length<f32>;
+    pub type LuminousIntensity = super::LuminousIntensity<f32>;
+    pub type Mass = super::Mass<f32>;
+    pub type ThermodynamicTemperature = super::ThermodynamicTemperature<f32>;
+    pub type Time = super::Time<f32>;
+    pub type Velocity = super::Velocity<f32>;
+}
+
+pub mod f64 {
+    pub type AmountOfSubstance = super::AmountOfSubstance<f64>;
+    pub type ElectricCurrent = super::ElectricCurrent<f64>;
+    pub type Length = super::Length<f64>;
+    pub type LuminousIntensity = super::LuminousIntensity<f64>;
+    pub type Mass = super::Mass<f64>;
+    pub type ThermodynamicTemperature = super::ThermodynamicTemperature<f64>;
+    pub type Time = super::Time<f64>;
+    pub type Velocity = super::Velocity<f64>;
+}
