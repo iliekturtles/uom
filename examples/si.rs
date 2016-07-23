bce11cb2146b13f36ba1131c0b1994604d609f83
Mike Boutin <mike.boutin@gmail.com>
2016-07-23 13:30:54 -0400

    Fix si.rs example.

--- examples/si.rs
+++ examples/si.rs
@@ -5,12 +5,14 @@ use std::default::{Default};
 use uom::si::f32::*;
 
 fn main() {
-    let mass = Length::default();
+    let mass = Mass::default();
+    let length = Length::default();
     let time = Time::default();
-    let velocity: Velocity = mass / time;
-    let more_mass = mass + mass;
-    //let error = mass + time; // mismatched types [E0308]
+    let velocity: Velocity = length / time;
+    let more_mass = length + length;
+    //let error = length + time; // mismatched types [E0308]
+    //let error: Velocity = mass / time; // mismatched types [E0308]
 
-    println!("{:?} / {:?} = {:?}", mass, time, velocity);
-    println!("{mass:?} + {mass:?} = {more_mass:?}", mass = mass, more_mass = more_mass);
+    println!("{:?} / {:?} = {:?}", length, time, velocity);
+    println!("{length:?} + {length:?} = {more_mass:?}", length = length, more_mass = more_mass);
 }
