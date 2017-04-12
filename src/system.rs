ffcf47dd8b6316c6bd20df7176864c9ea4718b8e
Mike Boutin <mike.boutin@gmail.com>
2017-04-11 20:13:26 -0400

    Add tests that rely on the standard library.
    Part of #22.

--- src/system.rs
+++ src/system.rs
@@ -200,8 +200,8 @@ macro_rules! system {
                 V: $crate::stdlib::fmt::Debug,
         {
             fn fmt(&self, f: &mut $crate::stdlib::fmt::Formatter) -> $crate::stdlib::fmt::Result {
-                if f.alternate() {
-                    write!(f, "{:#?}", self.value)
+                if let Some(precision) = f.precision() {
+                    write!(f, "{:.*?}", precision, self.value)
                 }
                 else {
                     write!(f, "{:?}", self.value)
