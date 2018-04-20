961d5219efd2a98571a1c562598fe5d1bba85d1b
Alex Hamilton <alex.hamilton@ou.edu>
2018-04-20 16:22:55 -0500

    Add mass rate quantity and associated units.
    Resolves #71.

--- src/si/mod.rs
+++ src/si/mod.rs
@@ -30,6 +30,7 @@ system! {
         length::Length,
         luminous_intensity::LuminousIntensity,
         mass::Mass,
+        mass_rate::MassRate,
         thermodynamic_temperature::ThermodynamicTemperature,
         time::Time,
         velocity::Velocity,
