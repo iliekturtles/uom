9b402ca51da32930348edd18a8c896f4c551a9a5
M@ Dunlap <mrd@vicarious.com>
2019-05-08 13:46:28 -0700

    Introduced AngleKind and si::marker, and added an implementation of From that converts between Kinds.

--- src/si/mod.rs
+++ src/si/mod.rs
@@ -70,3 +70,84 @@ storage_types! {
 
     ISQ!(si, V);
 }
+
+/// Primitive traits and types representing basic properties of types specific to the SI.
+pub mod marker {
+    use si::{change_base, Dimension, Quantity, Units};
+    use Kind;
+
+    /// AngleKind is a `Kind` for separating angular quantities from their indentically dimensioned
+    /// non-angular quantity counterparts. Conversions to and from `AngleKind` quantities is
+    /// supported through implementations of the `From` trait.
+    ///
+    #[cfg_attr(feature = "f32", doc = " ```rust")]
+    #[cfg_attr(not(feature = "f32"), doc = " ```rust,ignore")]
+    /// # use uom::si::f32::*;
+    /// # use uom::si::angle::radian;
+    /// let a: Angle = Angle::new::<radian>(1.0);
+    /// let r: Ratio = a.into();
+    /// ```
+    pub trait AngleKind: ::Kind {}
+
+    /// Kind of thermodynamic temperature.
+    pub trait TemperatureKind:
+        ::marker::Mul
+        + ::marker::MulAssign
+        + ::marker::Div
+        + ::marker::DivAssign
+        + ::marker::Rem
+        + ::marker::RemAssign
+    {
+    }
+
+    /// `impl_from` generates generic inter-Kind implementations of `From`.
+    #[macro_export]
+    macro_rules! impl_from {
+        ($a:ident, $b:ident) => {
+            impl<L, M, T, I, Th, N, J, Ul, Ur, V>
+                From<
+                    Quantity<
+                        Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = $a>,
+                        Ur,
+                        V,
+                    >,
+                >
+                for Quantity<
+                    Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = $b>,
+                    Ul,
+                    V,
+                >
+            where
+                Ul: Units<V> + ?Sized,
+                Ur: Units<V> + ?Sized,
+                V: ::num_traits::Num + ::Conversion<V>,
+            {
+                fn from(
+                    val: Quantity<
+                        Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = $a>,
+                        Ur,
+                        V,
+                    >,
+                ) -> Quantity<
+                    Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = $b>,
+                    Ul,
+                    V,
+                > {
+                    Self {
+                        dimension: ::lib::marker::PhantomData,
+                        units: ::lib::marker::PhantomData,
+                        value: change_base::<
+                            Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = $b>,
+                            Ul,
+                            Ur,
+                            V,
+                        >(&val.value),
+                    }
+                }
+            }
+        };
+    }
+
+    impl_from!(AngleKind, Kind);
+    impl_from!(Kind, AngleKind);
+}
