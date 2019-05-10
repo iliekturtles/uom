a66e99ce6ebd8965d69b294a0416582bc63c303a
M@ Dunlap <mrd@vicarious.com>
2019-05-10 11:57:46 -0700

    Add `AngularAcceleration`.

--- src/si/mod.rs
+++ src/si/mod.rs
@@ -38,6 +38,7 @@ system! {
         acceleration::Acceleration,
         amount_of_substance::AmountOfSubstance,
         angle::Angle,
+        angular_acceleration::AngularAcceleration,
         area::Area,
         available_energy::AvailableEnergy,
         capacitance::Capacitance,
