457713283369d6768f662a51252090ba6ff40f86
Mike Boutin <mike.boutin@gmail.com>
2019-05-13 16:15:47 -0400

    Release v0.23.0.

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! uom = "0.22.2"
+//! uom = "0.23.0"
 //! ```
 //!
 //! and this to your crate root:
@@ -66,7 +66,7 @@
 //! ```toml
 //! [dependencies]
 //! uom = {
-//!     version = "0.22.2",
+//!     version = "0.23.0",
 //!     default-features = false,
 //!     features = [
 //!         "autoconvert", # automatic base unit conversion.
