80ac0931529b6d47eb3f9a317ee161f02c600d90
Mike Boutin <mike.boutin@gmail.com>
2019-08-14 20:55:22 -0400

    [ci skip] Expand system!.

--- uom-macros/src/system/expand.rs
+++ uom-macros/src/system/expand.rs
@@ -1,10 +1,46 @@
 use super::*;
 use proc_macro2::TokenStream;
+use quote::quote;
 
 /// Expand the `system!` macro invocation to define the system of quantities and default system of
 /// units.
 pub(crate) fn expand(input: System) -> Result<TokenStream, syn::Error> {
-    drop(input);
+    let System {
+        // name_attributes,
+        // name,
+        // units_attributes,
+        // units,
+        base_quantities,
+        quantities,
+        ..
+    } = input;
+    let dimensions = base_quantities.iter().map(|bq| {
+        let attributes = &bq.attributes;
+        let symbol = &bq.symbol;
 
-    Ok(TokenStream::new())
+        quote! {
+            #(#attributes)*
+            type #symbol: typenum::Integer;
+        }
+    });
+    let mods = quantities.iter().filter_map(|q| {
+        if q.add_mod {
+            let module = &q.module;
+
+            Some(quote! { pub mod #module; })
+        } else {
+            None
+        }
+    });
+
+    let ts = quote! {
+        /// TODO
+        pub trait Dimension: Send + Sync {
+            #(#dimensions)*
+        }
+
+        #(#mods)*
+    };
+
+    Ok(ts)
 }
