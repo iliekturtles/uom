6b8dacd3ae91b01d45a7a44b176de5061e1b1652
Mike Boutin <mike.boutin@gmail.com>
2019-08-14 20:55:38 -0400

    [ci skip] Expand quantity!.

--- uom-macros/src/quantity/expand.rs
+++ uom-macros/src/quantity/expand.rs
@@ -1,9 +1,25 @@
 use super::*;
 use proc_macro2::TokenStream;
+use quote::quote;
 
 /// Expand the `quantity!` macro invocation to define the parsed quantity and associated units.
 pub(crate) fn expand(input: Quantity) -> Result<TokenStream, syn::Error> {
-    drop(input);
+    let Quantity {
+        name_attributes,
+        name,
+        // description,
+        // dimension_attributes,
+        // system,
+        // dimensions,
+        // kind,
+        // units,
+        ..
+    } = input;
 
-    Ok(TokenStream::new())
+    let ts = quote! {
+        #(#name_attributes)*
+        pub type #name = uom::Quantity;
+    };
+
+    Ok(ts)
 }
