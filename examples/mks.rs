23287ba2ae1777c69093dd9f0c67fe1c75b1a3ea
Mike Boutin <mike.boutin@gmail.com>
2020-07-29 16:12:10 -0400

    Change examples to use `Display` instead of `Debug`.
    mks and si examples are updated to use `format_args` or
    `into_format_args` as appropriate so that the `Display` trait can be
    used to display quantities in specific units. Part of #123.

--- examples/mks.rs
+++ examples/mks.rs
@@ -4,16 +4,15 @@
 extern crate uom;
 
 use length::{foot, meter};
+use uom::fmt::DisplayStyle::Abbreviation;
 
 fn main() {
     let l1 = f32::Length::new::<meter>(100.0);
 
     println!(
-        "{:?} {} = {:?} {}",
-        l1.get::<meter>(),
-        meter::abbreviation(),
-        l1.get::<foot>(),
-        foot::abbreviation()
+        "{} = {}",
+        l1.into_format_args(meter, Abbreviation),
+        l1.into_format_args(foot, Abbreviation)
     );
 }
 
