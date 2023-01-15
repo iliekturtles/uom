725a12f682155363014481d62a2705c8becb46c2
eagle941 <aquila941@hotmail.it>
2023-01-15 14:49:45 +0000

    Add fixes to liter per minute

--- src/si/volume_rate.rs
+++ src/si/volume_rate.rs
@@ -77,7 +77,6 @@ quantity! {
         @decaliter_per_second: prefix!(milli) * prefix!(deca); "daL/s", "decaliter per second",
             "decaliters per second";
         @liter_per_second: prefix!(milli); "L/s", "liter per second", "liters per second";
-        @liter_per_minute: prefix!(milli) / 60.; "L/min", "liter per minute", "liters per minute";
         @deciliter_per_second: prefix!(milli) * prefix!(deci); "dL/s", "deciliter per second",
             "deciliters per second";
         @centiliter_per_second: prefix!(milli) * prefix!(centi); "cL/s", "centiliter per second",
@@ -98,6 +97,7 @@ quantity! {
             "zeptoliters per second";
         @yoctoliter_per_second: prefix!(milli) * prefix!(yocto); "yL/s", "yoctoliter per second",
             "yoctoliters per second";
+        @liter_per_minute: prefix!(milli) / 6.0_E1; "L/min", "liter per minute", "liters per minute";
 
         @acre_foot_per_second: 1.233_489_E3; "ac · ft/s", "acre-foot per second",
             "acre-feet per second";
