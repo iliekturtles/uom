313bf68bcb5d45d3a7f9a5e03e66abfbb2648344
Mike Boutin <mike.boutin@gmail.com>
2024-01-27 21:10:18 -0500

    Increase MSRV to 1.65.0.

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@
 //! [orbiter]: https://en.wikipedia.org/wiki/Mars_Climate_Orbiter
 //!
 //! ## Usage
-//! `uom` requires `rustc` 1.60.0 or later. Add this to your `Cargo.toml`:
+//! `uom` requires `rustc` 1.65.0 or later. Add this to your `Cargo.toml`:
 //!
 //! ```toml
 //! [dependencies]
