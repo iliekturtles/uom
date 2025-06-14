004d3ba5dbae9d23ef36ed36a887cb353476d635
elongatedsquid <malteaxner@gmail.com>
2025-06-14 22:38:46 +0200

    Add ancient Roman units of area.

--- src/si/area.rs
+++ src/si/area.rs
@@ -65,13 +65,41 @@ quantity! {
         @square_inch: 6.451_6_E-4; "in²", "square inch", "square inches";
         @square_mile: 2.589_988_110_336_E6; "mi²", "square mile", "square miles";
         @square_yard: 8.361_273_6_E-1; "yd²", "square yard", "square yards";
+
+        // Ancient Roman units.
+        @saltus: 2.018_672_64_E6; "saltus", "saltus", "saltus";
+        @centuria: 5.046_681_6_E5; "centuria", "centuria", "centuriae";
+        @heredium: 5.046_681_6_E3; "heredium", "heredium", "heredia";
+        @jugerum: 2.523_340_8_E3; "jugerum", "jugerum", "jugera";
+        @deunx: 2.313_062_4_E3; "deunx", "deunx", "deunces";
+        @dextans: 2.102_784_E3; "dextans", "dextans", "dextantes";
+        @dodrans: 1.892_505_6_E3; "dodrans", "dodrans", "dodrantes";
+        @bes: 1.682_227_2_E3; "bes", "bes", "besses";
+        @septunx: 1.471_948_8_E3; "septunx", "septunx", "septunces";
+        @semis: 1.261_670_4_E3; "semis", "semis", "semisses";
+        @actus_quadratus: 1.261_670_4_E3; "actus quadratus", "actus quadratus", "acta quadratus";
+        @quincunx: 1.051_392_E3; "quincunx", "quincunx", "quincunces";
+        @triens: 8.411_136_E2; "triens", "triens", "trientes";
+        @quadrans: 6.308_352_E2; "quadrans", "quadrans", "quadrantes";
+        @sextans: 4.205_568_E2; "sextans", "sextans", "sextantes";
+        @clima: 3.154_176_E2; "clima", "clima", "climata";
+        @uncia: 2.102_784_E2; "uncia", "uncia", "unciae";
+        @semiuncia: 1.051_392_E2; "semiuncia", "semiuncia", "semiunciae";
+        @sicilicus: 5.256_96_E1; "sicilicus", "sicilicus", "siculi";
+        @actus_simplex: 4.205_568_E1; "actus simplex", "actus simplex", "acta simplex";
+        @sextula: 3.504_64_E1; "sextula", "sextula", "sextulae";
+        @duo_scrupulum: 1.752_32_E1; "duo scrupulum", "duo scrupulum", "duo scrupula";
+        @scrupulum: 8.761_6_E0; "scrupulum", "scrupulum", "scrupula";
+        @dimidium_scrupulum: 4.380_8_E0; "dimidium scrupulum", "dimidium scrupulum",
+            "dimidium scupula";
+        @pes_quadratus: 8.761_6_E-2; "pes quadratus", "pes quadratus", "pedes quadratus";
     }
 }
 
 #[cfg(test)]
 mod tests {
     storage_types! {
-        use crate::num::One;
+        use crate::num::{FromPrimitive, One};
         use crate::si::area as a;
         use crate::si::length as l;
         use crate::si::quantities::*;
@@ -114,11 +142,45 @@ mod tests {
             test::<l::mile, a::square_mile>();
             test::<l::yard, a::square_yard>();
 
+            test::<l::pes, a::pes_quadratus>();
+
             fn test<L: l::Conversion<V>, A: a::Conversion<V>>() {
                 if A::is_valid() {
-                    Test::assert_eq(&Area::new::<A>(V::one()),
+                    Test::assert_approx_eq(&Area::new::<A>(V::one()),
                         &(Length::new::<L>(V::one()) * Length::new::<L>(V::one())));
                 }
+            }
+        }
+
+        #[test]
+        fn check_roman() {
+            test::<a::scrupulum>(1.0_E2);
+            test::<a::duo_scrupulum>(2.0_E2);
+            test::<a::sextula>(4.0_E2);
+            test::<a::actus_simplex>(4.8_E2);
+            test::<a::sicilicus>(6.0_E2);
+            test::<a::semiuncia>(1.2_E3);
+            test::<a::uncia>(2.4_E3);
+            test::<a::clima>(3.6_E3);
+            test::<a::sextans>(4.8_E3);
+            test::<a::quadrans>(7.2_E3);
+            test::<a::triens>(9.6_E3);
+            test::<a::quincunx>(1.2_E4);
+            test::<a::actus_quadratus>(1.44_E4);
+            test::<a::semis>(1.44_E4);
+            test::<a::septunx>(1.68_E4);
+            test::<a::bes>(1.92_E4);
+            test::<a::dodrans>(2.16_E4);
+            test::<a::dextans>(2.4_E4);
+            test::<a::deunx>(2.64_E4);
+            test::<a::jugerum>(2.88_E4);
+            test::<a::heredium>(5.76_E4);
+            test::<a::centuria>(5.76_E6);
+            test::<a::saltus>(2.304_E7);
+
+            fn test<A: a::Conversion<V>>(p: f64) {
+                Test::assert_approx_eq(&Area::new::<A>(V::one()),
+                    &Area::new::<a::pes_quadratus>(V::from_f64(p).unwrap()));
             }
         }
     }
