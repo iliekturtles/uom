64dede6d02873466fcb26a55d5f05624c7a0b3c7
Hara Red <rtc6fg4.fejg2@gmail.com>
2025-07-15 14:02:04 +0300

    Add libm dependency
    This commit adds the `libm` optional feature (not enabled by default) to
    turn the corresponding feature in the `num-traits` dependency crate. The
    commit updates some default SI units and changes `Float` exporting
    rules.
    
    The `libm` crate enables math support for `#![no_std]` crates,
    including trigonometric functions. Those are implemented for `Angle` and
    `Ratio` quantities, and now are supported and usable in a wider area of
    cases.

--- src/si/angle.rs
+++ src/si/angle.rs
@@ -1,6 +1,6 @@
 //! Angle (dimensionless quantity).
 
-#[cfg(feature = "std")]
+#[cfg(any(feature = "std", feature = "libm"))]
 use super::ratio::Ratio;
 
 quantity! {
@@ -64,7 +64,7 @@ impl Angle<crate::si::SI<f64>, f64> {
 }
 
 /// Implementation of various stdlib trigonometric functions
-#[cfg(feature = "std")]
+#[cfg(any(feature = "std", feature = "libm"))]
 impl<U, V> Angle<U, V>
 where
     U: crate::si::Units<V> + ?Sized,
