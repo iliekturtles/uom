ba7a35bec4cef163687b4e29a30cb3b1b4a8656f
Nicholas Nethercote <n.nethercote@gmail.com>
2025-08-11 08:45:17 +1000

    Put `@foo` rules into separate macros.
    `@foo` rules are common, but they have some problems. This commit
    moves the foo rules into separate macros, giving several benefits.
    
    - The `@foo` rules no longer appear in the docs, removing the need for
      the "`@...` match arms are considered private" sentences.
    
    - I find the code easier to read this way.
    
    - It speeds up compilation because there are fewer rules to try for each
      macro, though this does not have a noticeable effect for this crate.
    
    In each case, I named the new macro `foo_bar!` where `foo!` was the
    original macro name and `@bar` was the tag.

--- src/system.rs
+++ src/system.rs
@@ -1,5 +1,4 @@
-/// Macro to implement a [system of quantities](https://jcgm.bipm.org/vim/en/1.3.html). `@...` match
-/// arms are considered private.
+/// Macro to implement a [system of quantities](https://jcgm.bipm.org/vim/en/1.3.html).
 ///
 /// * `$quantities_attr`: System of quantities attributes. Generally used to set documentation
 ///   comments for the system of quantities.
@@ -267,7 +266,7 @@ macro_rules! system {
         }
 
         // Type alias for dimensions where all exponents of the factors are the given value.
-        type DN<N> = dyn Dimension<$($symbol = system!(@replace $symbol N),)+
+        type DN<N> = dyn Dimension<$($symbol = system_replace!($symbol N),)+
             Kind = dyn $crate::Kind>;
 
         /// Type alias for [dimension one][one] for which all the exponents of the factors
@@ -1626,12 +1625,17 @@ macro_rules! system {
                 pub type $quantity = __system::$module::$quantity<__system::$units<$V>, $V>;)+
             };
             ($path:path, $V:ty, $U:tt) => {
-                system!(@quantities $path, $V; $($name),+; $U; $($module::$quantity),+);
+                system_quantities!($path, $V; $($name),+; $U; $($module::$quantity),+);
             };
         }
     };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! system_quantities {
     (
-        @quantities $path:path,
+        $path:path,
         $V:ty;
         $($name:ident),+;
         ($($U:ident),+);
@@ -1652,5 +1656,12 @@ macro_rules! system {
         #[allow(unused_qualifications)]
         pub type $quantity = __system::$module::$quantity<Units, $V>;)+
     };
-    (@replace $_t:tt $sub:ty) => { $sub };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! system_replace {
+    ($_t:tt $sub:ty) => {
+        $sub
+    };
 }
